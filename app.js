e22e46a4271c6b05edfdb0b6c21d4849a7cde1a9
wei <a1234567045@yahoo.com.tw>
2021-05-13 20:06:46 +0800

    feat: create new.handlebars

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const app = express()
 const port = 3000
 const Restaurant = require('./models/restaurant')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })// 設定連線到 mongoDB
+const bodyParser = require('body-parser')
 const db = mongoose.connection
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
+
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -13,25 +25,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// require express-handlebars here
-const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
-
-// setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-// set static files
-app.use(express.static('public'))
-
-
 // routes setting
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
-  // res.render('index', { restaurants: restaurantList.results });
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
@@ -40,19 +39,28 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  if (req.body.image.length === 0) { req.body.image = 'https://www.teknozeka.com/wp-content/uploads/2020/03/wp-header-logo-33.png' }
+  const restaurant = req.body
+  return Restaurant.create({ restaurant }) // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
-    // return restaurant.title?.includes(keyword)
     return restaurant.name && restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
 
   res.render('index', { restaurants: restaurants });
 })
 
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
-})
+
 
 
 
