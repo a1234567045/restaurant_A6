988448335d53d7986d71ee0436ed5774b196775e
wei <a1234567045@yahoo.com.tw>
2021-05-13 16:53:26 +0800

    feat: add restaurant seeder

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })// 設定連線到 mongoDB
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
---
+++ models/restaurant.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restSchema = new Schema({
+  id: String,
+  name: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  name_en: String,
+  category: String,
+  image: String,
+  location: String,
+  phone: String,
+  google_map: String,
+  rating: String,
+  description: String,
+})
+module.exports = mongoose.model('Restaurant', restSchema)
